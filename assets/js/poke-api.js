8ad44b6d691fda77134a99241698d05d45182f80
leticiass76 <leticiasilvasantos76@gmail.com>
2023-12-07 13:40:18 -0300

    Desafio Pokedex finalizado

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,11 +1,11 @@
 
 const pokeApi = {}
-
 function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
     pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
 
+    
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
 
@@ -14,18 +14,19 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
 
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
 
+    pokemon.urlDetail = pokeDetail.abilities[0].ability.url
+    pokemon.experience = pokeDetail.base_experience
+    pokemon.abilities = pokeDetail.abilities.map((ability) => ability.ability.name)
+
     return pokemon
 }
-
 pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
         .then((response) => response.json())
         .then(convertPokeApiDetailToPokemon)
 }
-
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
